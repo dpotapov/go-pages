34e22b7bed9e9b0fea028aed8e32e44def40b76e
Daniel Potapov <potapovdaniel@gmail.com>
2024-10-02 14:43:04 -0500

    chtml: fix tests

--- chtml/example_test.go
+++ chtml/example_test.go
@@ -3,38 +3,20 @@
 // license that can be found in the LICENSE file.
 
 // This example demonstrates parsing HTML data and walking the resulting tree.
-package chtml_test
+package chtml
 
 import (
 	"fmt"
-	"log"
 	"strings"
-
-	"golang.org/x/net/html"
 )
 
-func ExampleParse() {
-	s := `<p>Links:</p><ul><li><a href="foo">Foo</a><li><a href="/bar/baz">BarBaz</a></ul>`
-	doc, err := html.Parse(strings.NewReader(s))
+func Example() {
+	s := "<html><body><p>Hello World</p></body></html>"
+	r := strings.NewReader(s)
+	docNode, err := Parse(r, nil)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "a" {
-			for _, a := range n.Attr {
-				if a.Key == "href" {
-					fmt.Println(a.Val)
-					break
-				}
-			}
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
-		}
-	}
-	f(doc)
-	// Output:
-	// foo
-	// /bar/baz
+
+	fmt.Println(docNode)
 }
